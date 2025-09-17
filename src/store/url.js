03983b2c60b0cae99f069f6e6433e6e6028a3b20
cherent tsegaye <abenigas59@gmail.com>
2025-09-17 14:55:28 +0300

    added proxy

--- src/store/url.js
+++ src/store/url.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useUrl = defineStore("url", {
   state: () => ({
-    url: "http://localhost:5000",
-    // url: "https://retail.gondarmenu.com",
+    // url: "http://localhost:5000",
+    url: "https://retail.gondarmenu.com",
   }),
 });
