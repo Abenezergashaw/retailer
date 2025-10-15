08574f29575491ab12503305dc6e253f3aa310a7
cherent tsegaye <abenigas59@gmail.com>
2025-10-15 11:13:32 +0300

    added unclaimed for andualem

--- src/components/BalanceTable.vue
+++ src/components/BalanceTable.vue
@@ -56,9 +56,12 @@ const selectedData = ref(null);
           <th class="px-4 py-2 text-left font-medium truncate">Redeemed</th>
           <th class="px-4 py-2 text-left font-medium truncate">Withdraws</th>
           <th class="px-4 py-2 text-left font-medium truncate">End Balance</th>
-          <!-- <th class="px-4 py-2 text-left font-medium truncate">
+          <th
+            class="px-4 py-2 text-left font-medium truncate"
+            v-if="auth?.user.createdBy == 'andualem'"
+          >
             Unclaimed Winnings
-          </th> -->
+          </th>
           <!-- <th class="px-4 py-2 text-left font-medium text-gray-700">
             Commission
           </th> -->
@@ -107,9 +110,12 @@ const selectedData = ref(null);
             Br.
             {{ (data.bets - data.cancellations - data.redeemed).toFixed(2) }}
           </td>
-          <!-- <td class="px-4 py-2 text-left truncate">
+          <td
+            class="px-4 py-2 text-left truncate"
+            v-if="auth?.user.createdBy == 'andualem'"
+          >
             Br. {{ data.unclaimed.toFixed(2) }}
-          </td> -->
+          </td>
         </tr>
       </tbody>
     </table>
